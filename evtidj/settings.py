e9a5801ba282b74695c4e04591605cb5fef9fe20
hnguyene <huy.nguyen@eventure.com>
2015-07-30 14:36:11 -0700

    add settings.LOGIN_URL

--- evtidj/settings.py
+++ evtidj/settings.py
@@ -240,8 +240,8 @@ BROKER_TRANSPORT_OPTIONS = {
 HOST_NAME = socket.gethostname()
 
 SITE_URL = 'http://devapi.eventure.com/'
-LOGIN_URL = 'fe:login'
 
+LOGIN_URL = '/e/login'
 
 GOOGLE_API_CLIENT_ID = '943142708269.apps.googleusercontent.com'
 GOOGLE_API_CLIENT_SECRET = '2ZIndLAernSicPFHgsi45_0s'
