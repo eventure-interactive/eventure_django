a3f4e585afa4fd46e237ce7e923fdf009b2d0f1a
hnguyene <huy.nguyen@eventure.com>
2015-08-31 10:15:30 -0700

    fix migration conflict in previous commit

---
+++ core/migrations/0071_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0070_merge'),
+        ('core', '0070_auto_20150826_1820'),
+    ]
+
+    operations = [
+    ]
